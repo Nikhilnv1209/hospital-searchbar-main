a3e1f7c2a560e7c02f8a5b25d0b88292c5f10d1b
Nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-01-30 23:51:30 +0530

    Refactor: Hospital Componen (Main screen for maps)

--- src/components/Hospitals/Hospitals.js
+++ src/components/Hospitals/Hospitals.js
@@ -1,7 +1,5 @@
-import React, {useContext} from "react";
-import { Outlet , useNavigate} from "react-router-dom";
-import styles from "./Hospitals.module.scss";
-// import Maps from "./Maps";
+import React, { useContext } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { HospitalContext } from "../../Context/HospitalContext";
 import List from "./List";
 
@@ -9,30 +7,26 @@ const Hospitals = () => {
   // eslint-disable-next-line
   const { setparams, setarea } = useContext(HospitalContext).data.setstate;
   const navigate = useNavigate();
-
   return (
-    <>
-        {/* <Maps /> */}
-        <div className={styles.HospitalList}>
-          <List />
-        </div>
-        <button
-          className={styles.backButton}
-          onClick={() => {
-            setparams({
-              lat: 0,
-              lon: 0,
-              place_id: "",
-              radius: "",
-            });
-            setarea("");
-            navigate("/");
-          }}
-        >
-          Go back
-        </button>
-<Outlet />
-    </>
+    <main className="relative flex-1 flex">
+      <List />
+      <button
+        className={"absolute bottom-1 right-[50%] translate-x-[50%] bg-[#393E46]/80 hover:bg-[#393E46]/50 transition duration-150 text-[#eeeeee] h-8 w-24 rounded-md text-sm z-10"}
+        onClick={() => {
+          setparams({
+            lat: 0,
+            lon: 0,
+            place_id: "",
+            radius: "",
+          });
+          setarea("");
+          navigate("/");
+        }}
+      >
+        Go back
+      </button>
+      <Outlet />
+    </main>
   );
 };
 
